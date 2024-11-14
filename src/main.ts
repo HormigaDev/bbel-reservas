9a19305ffe03ee4e76487cb7dd9e20eb7be8ed0c
Jot4Ce <asdjkl.key@gmail.com>
2024-11-13 21:30:36 -0300

    feat: ajustando algunas cosas para produccion

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,13 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
+
+    app.enableCors({
+        origin: 'http://localhost:3000',
+        methods: 'GET,POST,PUT,DELETE,PATCH',
+        credentials: true,
+    });
+
     app.useGlobalPipes(
         new ValidationPipe({
             transform: true,
@@ -12,6 +19,6 @@ async function bootstrap() {
             disableErrorMessages: false,
         }),
     );
-    await app.listen(3000);
+    await app.listen(+process.env.PORT || 4000);
 }
 bootstrap();
